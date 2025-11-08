71dda6c4a2b80576bb7a096a2e397d9dc0103187
Pstas <ballabtnd@gmail.com>
2025-11-08 23:05:46 +0100

    Reorder import statements for better readability

--- dusza25/asgi.py
+++ dusza25/asgi.py
@@ -10,13 +10,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from damareen.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dusza25.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from damareen.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
